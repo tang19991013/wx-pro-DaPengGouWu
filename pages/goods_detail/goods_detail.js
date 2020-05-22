94275be62cf61cf2f5f731b4e682052d77d9d538
大鹏 <1328310383@qq.com>
2020-05-22 12:27:11 +0800

    完成商品详情页面

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,18 +1,64 @@
-// pages/goods_detail/goods_detail.js
+import {request} from '../../request/index.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    proId:0,
+    proInfo:{},
   },
-
+getProInfo(){
+  request({url:'https://api-hmugo-web.itheima.net/api/public/v1/goods/detail',data:{
+    goods_id:this.data.proId
+  } 
+}).then(res=>{
+    this.setData({
+      proInfo:res.data.message
+    })
+  })
+},
+//单击轮播图 放大预览
+handlePrevewImage(e){
+  let urls=this.data.proInfo.pics.map(v=>v.pics_mid);
+  let current=e.currentTarget.dataset.url;
+  wx.previewImage({
+    current,
+    urls
+  });
+},
+//加入购物车
+addCart(){
+  //1获取缓存中购物车数数组
+  let cart=wx.getStorageSync("cart")||[];
+  //2判断商品对象手是存在于购物车数组中
+  let index=cart.findIndex(v=>v.goods_id===this.data.proInfo.goods_id)
+  if(index===-1){
+    //不存在，表示第一次添加
+    console.log("ss")
+    this.data.proInfo.num=1;
+    cart.push(this.data.proInfo);
+  }else{
+    //已经在购物车数据，执行num++
+   cart[index].num++;
+  }
+  //把购物车重新添加到缓存中
+  wx.setStorageSync('cart', cart)
+  //弹窗提示
+  wx.showToast({
+    title: '加入成功',
+    icon: 'success',
+    mask:true
+  })
+},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      proId:options.pid
+    })
+    this.getProInfo();
   },
 
   /**
